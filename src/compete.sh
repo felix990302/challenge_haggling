ca8c841f2db85ddee4c02e33ddd5c20df2f50647
Felix Zhou <felix990302@yahoo.ca>
2018-06-29 22:12:06 -0400

    renamed solution to negotiator and modified challenge script accordingly

--- src/compete.sh
+++ src/compete.sh
@@ -1,2 +1,2 @@
-while true; do node haggle.js --id cfzhou@uwaterloo.ca:990302 offer.js wss://hola.org/challenges/haggling/arena/standard; done
+while true; do node haggle.js --id cfzhou@uwaterloo.ca:990302 negotiator.js wss://hola.org/challenges/haggling/arena/standard; done
 
