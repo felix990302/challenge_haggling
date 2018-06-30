d76858df856adf46a913f8fdde12fd28415dcb23
Felix Zhou <felix990302@yahoo.ca>
2018-06-29 23:52:22 -0400

    updated compete script to accept arena, defaults to standard_1s

--- src/compete.sh
+++ src/compete.sh
@@ -1,2 +1,19 @@
-while true; do node haggle.js --id cfzhou@uwaterloo.ca:990302 negotiator.js wss://hola.org/challenges/haggling/arena/standard; done
+arena="standard_1s"
+if [ "$1" = "standard" ]
+then
+    arena=$1
+elif [ "$1" = "large"  ]
+then
+    arena=$1
+elif [ "$1" = "large_1s" ]
+then 
+    arena=$1
+fi
+
+echo "entering the arena: $arena"
+
+while true
+    do 
+        node haggle.js --id cfzhou@uwaterloo.ca:990302 negotiator.js wss://hola.org/challenges/haggling/arena/$arena
+    done
 
